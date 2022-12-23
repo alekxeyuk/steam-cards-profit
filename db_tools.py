3e685095a630ffb5ee95cd263b997628aba9d0f7
LeLuCh B0й <9300326+alekxeyuk@users.noreply.github.com>
2022-12-23 15:47:49 +0700

    Create db_tools.py

---
+++ db_tools.py
@@ -0,0 +1,48 @@
+from pprint import pprint
+
+from cassandra import InvalidRequest
+from cassandra.cqlengine.management import sync_table
+from cassandra.cqlengine.query import BatchQuery
+from termcolor import colored
+
+from db.connect import connect
+from db.models import SteamApp, TradingCard
+
+
+def main():
+    print(colored("Connecting to database...", "green"))
+    connect()
+
+    while user_input := input("What do you want? {sync / create / get }: "):
+        match user_input:
+            case "sync":
+                sync_table(SteamApp)
+                sync_table(TradingCard)
+                print(colored("Table synced", "green"))
+            case "create":
+                try:
+                    inst = SteamApp.create(appid=1, name="Test", url="https://test.ru", price=100, owned=True)
+                    print(colored(f"Created: {inst}", "green"))
+                except InvalidRequest as e:
+                    print(colored(f"Wrong request: {e}", "red"))
+            case "get":
+                print(SteamApp.objects.count())
+                pprint(SteamApp.objects.all()[:])
+                print(TradingCard.objects.count())
+                pprint(TradingCard.objects.all()[:])
+            case "nuke":
+                print(colored("Nuking DB", "red"))
+                for inst in SteamApp.objects.all():
+                    inst.delete()
+                for inst in TradingCard.objects.all():
+                    inst.delete()
+                print(colored("DB nuked", "red"))
+            case "exit":
+                print(colored("Bye", "green"))
+                break
+            case _:
+                print(colored(f"Command '{user_input}' not understood", "red"))
+
+
+if __name__ == "__main__":
+    main()
